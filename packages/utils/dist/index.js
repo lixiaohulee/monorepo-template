b6ad25d26eb9fe4cf981fae560ede2a3b6d6539b
lixiaohu <lixiaohu_neuq@163.com>
2025-07-20 10:52:59 +0800

    feat: update utils

--- packages/utils/dist/index.js
+++ packages/utils/dist/index.js
@@ -3,5 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var func = function () {
     console.log('func');
     console.log('test');
+    console.log('leee');
+    console.log('wang');
+    console.log('support');
 };
 exports.default = func;
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,6 +3,9 @@
 const func = (): void => {
     console.log('func')
     console.log('test')
+    console.log('leee')
+    console.log('wang')
+    console.log('support')
 }
 
 export default func;
